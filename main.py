b45ebb5d3b604dd4a267d8b7e5711b39b71e9562
temmyy <temmy.work.free@gmail.com>
2022-02-24 20:56:44 +0700

    working version

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import datetime
 import calendar
+import pdfkit
+from jinja2 import Environment, FileSystemLoader
 
 
 def init():
 
     p = {}
+    p['print_invoice'] = {}
     with open('config.txt') as config:
         for r in config.readlines():
             if r.startswith('apiKey'):
@@ -13,12 +16,15 @@ def init():
                 p['headers'] = {"Authorization": "Bearer " + p['apiKey'], "accept": "text/json"}
             elif r.startswith('salary'):
                 p['salary'] = int(r[7:-1])
+                p.get('print_invoice')['_salary_'] = r[7:-1]
             elif r.startswith('quantityInInvoice'):
                 p['quantityInInvoice'] = int(r[18:-1])
-        p['businessDay'] = int(input('Количество рабочих дней: '))
+            elif r.startswith('print_invoice'):
+                p.get('print_invoice')[r[14:r.find(' ', 15)]] = r[r.find(' ', 15):-1]
+        p['businessDay'] = 10# int(input('Количество рабочих дней: '))
 
-        year = str(input('Год в формате [2021]: '))
-        month = str(input('Месяц в формате [08]: '))
+        year = "2022"# str(input('Год в формате [2021]: '))
+        month = "01"# str(input('Месяц в формате [08]: '))
         start_date = string_to_date(year + '-' + month + '-01T00:00:00')
         days_in_month = calendar.monthrange(start_date.year, start_date.month)[1]
         end_date = start_date + datetime.timedelta(days=days_in_month) - datetime.timedelta(seconds=1)
@@ -27,11 +33,28 @@ def init():
         p['endDate'] = end_date.strftime('%Y-%m-%dT%H:%M:%S')
         p['amount_seconds'] = 0
 
+        p.get('print_invoice')['_invoice_year_'] = year
+        p.get('print_invoice')['_invoice_month_en_'] = get_month_name(month, 0)
+        p.get('print_invoice')['_invoice_month_ru_'] = get_month_name(month, 1)
+        p.get('print_invoice')['_invoice_day_'] = str(datetime.datetime.now().day)
+        p.get('print_invoice')['_invoice_number_'] = "022"# str(input('Номер инвойса [018]:'))
+
     return p
 
 
 def string_to_date(date_in_string):
     return datetime.datetime.strptime(date_in_string, "%Y-%m-%dT%H:%M:%S")
+
+
+def get_month_name(m, t=0):
+    month_ru = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня',
+                'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
+    month_en = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                'August', 'September', 'October', 'November', 'December']
+    if t == 0:
+        return month_en[int(m) - 1]
+    else:
+        return month_ru[int(m) - 1]
 
 
 def function_sort(element):
@@ -59,6 +82,18 @@ def distribute_on_best(q_best):
                 table_time.get(k[0])[0] = k[1]
 
 
+def get_project_description(id):
+    project = projects_description.get(id)
+    project_description = {}
+    if project is None:
+        project_description['name_ru'] = 'Неопределено (' + str(id) + ')'
+        project_description['name_en'] = 'None'
+    else:
+        project_description['name_ru'] = project[0]
+        project_description['name_en'] = project[1]
+    return project_description
+
+
 def print_table():
     print('_' * 76)
     print('Проект' + ' ' * (60 - len('Проект')), 'Доля', 'Дней', 'Деньги')
@@ -69,16 +104,12 @@ def print_table():
     for k in table_time.items():
         if k[1][0] == 0:
             continue
-        project = projects_description.get(k[0])
-        if project is None:
-            project_name_ru = 'None (' + str(k[0]) + ')'
-            project_name_en = ''
-        else:
-            project_name_ru = project[0]
-            project_name_en = project[1]
+        project_description = get_project_description(k[0])
         s1 += k[1][1]
         s2 += k[1][2]
         s3 += k[1][3]
+        project_name_ru = project_description.get('name_ru')
+        project_name_en = project_description.get('name_en')
         print(project_name_ru + ' ' * (60 - len(project_name_ru)),
               str(k[1][1]) + ' ',
               str(k[1][2]) + ' ',
@@ -100,7 +131,7 @@ def control_of_totals(p):
     last_item = list(table_time.values())[p['quantityInInvoice'] - 1]
     last_item[1] = round(last_item[1] + 1 - part, 2)
     last_item[2] = round((last_item[2] + p['businessDay'] - days), 2)
-    last_item[3] = round(last_item[1] * p['salary'], 0)
+    last_item[3] = int(last_item[1] * p['salary'])
 
 
 def get_time_entries(p):
@@ -126,7 +157,7 @@ def fill_table_time(p):
         delta_time = string_to_date(k.get('endTime')) - string_to_date(k.get('startTime'))
         seconds = delta_time.days * 24 * 60 * 60 + delta_time.seconds
         if project_id not in table_time:
-            table_time[project_id] = [0 for 0 in range(0, 4)]
+            table_time[project_id] = [0 for i in range(0, 4)]
         table_time[project_id][0] += seconds
         p['amount_seconds'] += seconds
 
@@ -138,6 +169,40 @@ def fill_projects_description():
                 continue
             line = text_line.strip().split('|')
             projects_description[int(line[0])] = [line[1], line[2]]
+
+
+def print_invoice_to_pdf(p):
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template("Invoice_template.html")
+    pdf_template = template.render(p.get('print_invoice'))
+
+    template_table_line = '''
+      <tr>
+        <td>{num}</td>
+        <td>{service_en}<br>{service_ru}</td>
+        <td>{amount}<br>(business days: {days}/{business_days})</td>
+        <td>{unit}</td>
+        <td>US${salary}</td>
+        <td align="right">US${total}</td>
+      <tr>'''
+    s = ''
+    i = 0
+    for k in table_time.items():
+        project_description = get_project_description(k[0])
+        i += 1
+        new_line = '''
+              <tr>
+                <td align="center">''' + str(i) + '''</td>
+                <td align="left"><b>''' + project_description.get('name_en') + '''</b><br>''' + project_description.get('name_ru') + '''</td>
+                <td align="left">''' + str(k[1][1]) + '''<br>(business days: ''' + str(k[1][2]) + '''/''' + str(p.get('businessDay')) + ''')</td>
+                <td align="center">Month<br>Месяц</td>
+                <td align="center">US$''' + str(p.get('salary')) + '''</td>
+                <td align="right">US$''' + str(k[1][3]) + '''</td>
+              <tr>'''
+        s += new_line
+    pdf_template = pdf_template.replace('<!-- table_line -->', s)
+
+    pdfkit.from_string(pdf_template, 'invoice.pdf')
 
 
 if __name__ == '__main__':
@@ -158,3 +223,5 @@ if __name__ == '__main__':
 
     control_of_totals(params)
     print_table()
+
+    print_invoice_to_pdf(params)
